08c8ca4bc7bb18cefed916fe4ae1167f25e8b298
Geoffroy Megert <geoffroy.megert@he-arc.ch>
2015-11-26 09:31:25 +0100

    Le Backing Bean pour afficher le détail d'un client est codé.

---
+++ src/java/ch/hearc/ig/odi/bankapp/presentation/bean/CustomerDetailBean.java
@@ -0,0 +1,51 @@
+package ch.hearc.ig.odi.bankapp.presentation.bean;
+
+import ch.hearc.ig.odi.bankapp.business.Account;
+import ch.hearc.ig.odi.bankapp.business.Customer;
+import ch.hearc.ig.odi.bankapp.services.Services;
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Geoffroy Megert <geoffroy.megert@he-arc.ch>
+ */
+@Named("CustomerDetailBean")
+@RequestScoped
+public class CustomerDetailBean implements Serializable {
+    
+    private Customer customer;
+    private DataModel<Account> accounts;
+    
+    @Inject
+    private Services services;
+
+    public String getCustomer(final int id) {
+        customer = services.getCustomer(id);
+        
+        return "success";
+    }
+    
+    public DataModel<Account> getAccounts() {
+        accounts = new ListDataModel<>();
+        
+        for(Account account : customer.getAccounts()) {
+            accounts.setWrappedData(account);
+        }
+        
+        return accounts;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    
+}
