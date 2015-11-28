826253c93d4c236127618218b5e7b04cda31552a
Geoffroy Megert <Geoffroy Megert@192.168.1.154>
2015-11-28 15:22:46 +0100

    Fichiers "customersList.xhtml" et son Backing Bean "CustomersBean.java" commentés.

--- src/java/ch/hearc/ig/odi/bankapp/presentation/bean/CustomersBean.java
+++ src/java/ch/hearc/ig/odi/bankapp/presentation/bean/CustomersBean.java
@@ -10,18 +10,31 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
- *
+ * Cette classe est un Backing Bean lié à la page "customersList.xhtml".
+ * 
  * @author Geoffroy Megert <geoffroy.megert@he-arc.ch>
  */
+// Le nom du Backing Bean. C'est ce nom qui est employé pour l'appeler depuis d'autres pages.
 @Named("CustomersBean")
+// Cycle de vie du Backing Bean. Il ne modifie pas d'objet, donc son cycle de vie est d'une requête.
 @RequestScoped
 public class CustomersBean implements Serializable {
     
+    /**
+     * La liste des clients enregistrés auprès d'une banque.
+     */
+    // Les DataModel sont orientés pour les composants des JavaServer Faces.
     private DataModel<Customer> customers;
     
+    // Gère automatiquement l'instanciation de l'attribut "services".
     @Inject
     private Services services;
     
+    /**
+     * Permet d'obtenir la liste des clients enregistrés auprès d'une banque.
+     * 
+     * @return La liste des clients enregistrés auprès d'une banque.
+     */
     public DataModel<Customer> getCustomers() {
         customers = new ListDataModel<>();
         customers.setWrappedData(services.getCustomersList());
