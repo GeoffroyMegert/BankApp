ee45ca1c8539f971c62da24e4a252f3c63b09cc6
Geoffroy Megert <Geoffroy Megert@192.168.1.154>
2015-11-30 16:51:41 +0100

    Fichier "createAccount.xhtml" et son Backing Bean "AccountCreateBean.java" commenté. Une méthode pour l'ajout de compte dans la banque a été ajouté à la classe"Services.java".

--- src/java/ch/hearc/ig/odi/bankapp/presentation/bean/AccountCreateBean.java
+++ src/java/ch/hearc/ig/odi/bankapp/presentation/bean/AccountCreateBean.java
@@ -2,26 +2,48 @@ package ch.hearc.ig.odi.bankapp.presentation.bean;
 
 import ch.hearc.ig.odi.bankapp.business.Account;
 import ch.hearc.ig.odi.bankapp.business.Customer;
+import ch.hearc.ig.odi.bankapp.services.Services;
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
+ * Cette classe est un Backing Bean lié à la page "createAccount.xhtml".
+ * Elle permet de gérer la création de compte.
  *
  * @author Geoffroy Megert <geoffroy.megert@he-arc.ch>
  */
+// Le nom du Backing Bean. C'est ce nom qui est employé pour l'appeler depuis d'autres pages.
 @Named("AccountCreateBean")
+// Cycle de vie du Backing Bean.
+// Il modifie des objets qui ont besoins de persister plus d'une requête, donc son cycle de vie est d'une session.
 @SessionScoped
 public class AccountCreateBean implements Serializable {
     
+    /** Le numéro du compte. */
     private String number;
+    /** Le nom du compte. */
     private String name;
+    /** Le solde du compte. */
     private double balance;
+    /** Le taux d'intérêt du compte. */
     private double rate;
+    /** Le client propriétaire du nouveau compte. */
     private Customer customer;
     
+    /** Services mises à disposition du programmeur. */
+    // Gère automatiquement l'instanciation de l'attribut "services".
+    @Inject
+    private Services services;
+    
+    /**
+     * Effectuer l'ouverture du nouveau compte auprès de la banque.
+     * 
+     * @return "success" si le compte a pu être ouvert, sinon retourne "failure".
+     */
     public String submit() {
-        Account account = customer.addAccount(number, name, rate);
+        Account account = services.saveAccount(number, name, rate, customer);
         
         if(account != null) {
             account.setBalance(balance);
@@ -31,6 +53,12 @@ public class AccountCreateBean implements Serializable {
         }
     }
     
+    /**
+     * Reçoit le futur propriétaire du nouveau compte.
+     * 
+     * @param customer Le futur propriétaire du compte.
+     * @return "success" si le client a été reçu, sinon retourne "failure".
+     */
     public String getCustomer(final Customer customer) {
         if(customer != null) {
             this.customer = customer;
@@ -40,42 +68,92 @@ public class AccountCreateBean implements Serializable {
         }
     }
 
+    /**
+     * Permet d'obtenir le numéro du nouveau compte.
+     * 
+     * @return Le numéro du compte.
+     */
     public String getNumber() {
         return number;
     }
 
+    /**
+     * Modifie le numéro du compte par celui passé en paramètre.
+     * 
+     * @param number Le nouveau numéro du compte.
+     */
     public void setNumber(final String number) {
         this.number = number;
     }
 
+    /**
+     * Permet d'obtenir le nom du nouveau compte.
+     * 
+     * @return Le nom du compte.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Modifie le nom du compte par celui passé en paramètre.
+     * 
+     * @param name Le nouveau nom du compte.
+     */
     public void setName(final String name) {
         this.name = name;
     }
 
+    /**
+     * Permet d'obtenir le solde du nouveau compte.
+     * 
+     * @return Le solde du compte.
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Modifie le solde du compte par celui passé en paramètre.
+     * 
+     * @param balance Le nouveau solde du compte.
+     */
     public void setBalance(final double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Permet d'obtenir le taux d'intérêt du nouveau compte.
+     * 
+     * @return Le taux d'intérêt du compte.
+     */
     public double getRate() {
         return rate;
     }
 
+    /**
+     * Modifie le taux d'intérêt du compte par celui passé en paramètre.
+     * 
+     * @param rate Le nouveau taux d'intérêt du compte.
+     */
     public void setRate(final double rate) {
         this.rate = rate;
     }
 
+    /**
+     * Permet d'obtenir le futur propriétaire du nouveau compte.
+     * 
+     * @return Le futur propriétaire du nouveau compte.
+     */
     public Customer getCustomer() {
         return customer;
     }
 
+    /**
+     * Modifie le propriétaire du nouveau compte par celui passé en paramètre.
+     * 
+     * @param customer Le nouveau propriétaire du compte.
+     */
     public void setCustomer(final Customer customer) {
         this.customer = customer;
     }
